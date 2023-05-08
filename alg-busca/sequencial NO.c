b088f74658d2863b3ee2b4f13a1be586ceb96cc7
mayarakaren <mayarakaren5910@gmail.com>
2023-05-07 23:32:25 -0300

    V9

---
+++ alg-busca/sequencial NO.c
@@ -0,0 +1,13 @@
+#include <iostream.h>
+#include <conio.h>
+
+void main(){
+    int x[10], n, i, achou;
+    clrscr();
+    //carregando os números no vetor
+    //vetor com números não ordenados
+    for(i=0; i<=9; i++){
+        cout<<"Digite o"<<i+1<<"° número:";
+        cin>>x[i];
+    }
+}
